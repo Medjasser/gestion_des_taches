ce8d02da045a6b13253c89d89f97ec08b483298d
Medjasser <saidanejassermohamed@gmail.com>
2022-05-22 10:04:57 +0100

    Theme et notification

---
+++ lib/projet/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class Themes{
+
+  static final light= ThemeData(
+      primaryColor: Colors.blue,
+      brightness: Brightness.light
+  );
+  static final dark= ThemeData(
+      primaryColor: Colors.black87,
+      brightness: Brightness.dark
+
+  );
+}
